6684f9631474645e4b8e5372ac7affb4197dda28
crewd <newspaper321@nate.com>
2022-07-06 22:00:01 +0900

    fix: 날짜 포맷팅

--- src/components/post/PostCard.tsx
+++ src/components/post/PostCard.tsx
@@ -1,9 +1,21 @@
 import { Post } from "../../types/postType";
 
 const PostCard = ({ message, author, date }: Post) => {
+  const dateParse = Date.parse(date.substring(0, 10));
+  const newDate = new Date(dateParse);
+
+  const year = newDate.getFullYear();
+  const month = ('0' + (newDate.getMonth() + 1)).slice(-2);
+  const day = ('0' + newDate.getDate()).slice(-2);
+
+  const commitDate = year + '-' + month + '-' + day;
+
+
   return (
     <li className="post-card">
-      {message} {author} {date}
+      <p className="post-card__title">{message}</p>
+      <p className="post-card__date">{commitDate.toString()}</p>
+      <p className="post-card__author">{author}</p>
     </li>
   )
 }
