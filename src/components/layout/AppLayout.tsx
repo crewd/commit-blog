d0d5a546bb6ea1dd48587ffbdcb7cf5606b57ea6
crewd <newspaper321@nate.com>
2022-07-07 02:35:05 +0900

    style: 헤더 디자인 추가

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -4,13 +4,15 @@ const AppLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       <header className="header">
-        <h1 className="header-title">Blog</h1>
-        <nav className="header-nav">
-          <ul>
-            <li>검색</li>
-            <li>글 작성</li>
-          </ul>
-        </nav>
+        <div className="header-content">
+          <h1 className="header-title">Blog</h1>
+          <nav className="header-nav">
+            <ul>
+              <li>검색</li>
+              <li>글 작성</li>
+            </ul>
+          </nav>
+        </div>
       </header>
       <main className="content">
         {children}
