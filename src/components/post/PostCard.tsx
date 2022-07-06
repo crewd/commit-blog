932391eb3584f7624640d2293a96583eae81b663
crewd <newspaper321@nate.com>
2022-07-06 22:15:43 +0900

    style: post card hover시 애니메이션 추가

--- src/components/post/PostCard.tsx
+++ src/components/post/PostCard.tsx
@@ -1,12 +1,11 @@
 import { Post } from "../../types/postType";
 
 const PostCard = ({ message, author, date }: Post) => {
-  const dateParse = Date.parse(date.substring(0, 10));
-  const newDate = new Date(dateParse);
+  const getCommitDate = new Date(date);
 
-  const year = newDate.getFullYear();
-  const month = ('0' + (newDate.getMonth() + 1)).slice(-2);
-  const day = ('0' + newDate.getDate()).slice(-2);
+  const year = getCommitDate.getFullYear();
+  const month = ('0' + (getCommitDate.getMonth() + 1)).slice(-2);
+  const day = ('0' + getCommitDate.getDate()).slice(-2);
 
   const commitDate = year + '-' + month + '-' + day;
 
