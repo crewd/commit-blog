ee67d8780b5f320513e9cf33ae4e4d63da49d347
crewd <newspaper321@nate.com>
2022-07-15 20:11:18 +0900

    feat: 마크다운 에디터 세팅

--- src/pages/CreatePostPage.tsx
+++ src/pages/CreatePostPage.tsx
@@ -1,5 +1,28 @@
+import Prism from 'prismjs';
+// 여기 css를 수정해서 코드 하이라이팅 커스텀 가능
+import 'prismjs/themes/prism.css';
+
+import '@toast-ui/editor/dist/toastui-editor.css';
+import { Editor } from '@toast-ui/react-editor';
+
+import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
+import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight';
+
+import 'tui-color-picker/dist/tui-color-picker.css';
+import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
+import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
+
+
 const CreatePostPage = () => {
-  return <div>글 작성 페이지</div>
+  return (
+    <Editor
+      initialValue=' # 제목
+      <br>
+      내용'
+      previewStyle='vertical'
+      plugins={[colorSyntax, [codeSyntaxHighlight, { highlighter: Prism }]]}
+    />
+  )
 }
 
 export default CreatePostPage;
