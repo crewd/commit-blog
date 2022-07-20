cd66a691394b94efa131fcb1144bbe25b103fe54
crewd <newspaper321@nate.com>
2022-07-21 01:58:19 +0900

    fix: gitignore 설정

--- src/react-app-env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react-scripts" />
-declare namespace NodeJS {
-  interface ProcessEnv {
-    NODE_ENV: "development" | "production" | "test";
-    PUBLIC_URL: string;
-    REACT_APP_GIT_TOKEN: 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;
-  }
-}
